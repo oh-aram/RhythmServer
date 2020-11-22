fae8cdf0d4203b216301705bb8243cb33024f1c0
oh-aram <oalam2@naver.com>
2020-11-23 05:12:14 +0900

    진짜

---
+++ src/rhythmgame/ChatMsg.java
@@ -0,0 +1,62 @@
+package rhythmgame;
+// ChatMsg.java 채팅 메시지 ObjectStream 용.
+import java.io.Serializable;
+import javax.swing.ImageIcon;
+
+class ChatMsg implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String id;
+	private String code; // 100:로그인, 400:로그아웃, 200:채팅메시지, 300:Image
+	private String data;
+	public ImageIcon img;
+	
+	private int num;
+	
+	//커밋을 위한 주석
+	public ChatMsg(String id, String code) {
+		this.id = id;
+		this.code = code;
+	}
+
+	public ChatMsg(String id, String code, String msg) {
+		this.id = id;
+		this.code = code;
+		this.data = msg;
+	}
+	
+	public int getNum() {
+		return num;
+	}
+	
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public void setImg(ImageIcon img) {
+		this.img = img;
+	}
+}
